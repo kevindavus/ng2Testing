db7c5d606ac989ca8e35ac86d1d1f8c01cbf2e88
Kevin <osfan501@gmail.com>
2016-12-06 16:43:09 -0500

    cleanup

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './courses.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,17 +10,17 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, courses_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (courses_component_1_1) {
+                courses_component_1 = courses_component_1_1;
             }],
         execute: function() {
-            (CoursesComponent);
-            from;
-            './courses.component';
             AppComponent = (function () {
                 function AppComponent() {
                 }
@@ -28,7 +28,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                     core_1.Component({
                         selector: 'my-app',
                         template: '<h1>My First Angular 2 App</h1><courses></courses>',
-                        directives: [CoursesComponent]
+                        directives: [courses_component_1.CoursesComponent]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/courses.component.ts
+++ app/courses.component.ts
@@ -2,9 +2,16 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h1>'
+    template: `
+    <h2>Courses</h2>
+    {{title}}
+    <ul>
+      <li *ngFor="#course of courses">
+      {{course}}</li>
+    </ul>
+    `
 })
 export class CoursesComponent {
-
-
+  title = "The title of the courses page";
+  courses = ["Course1", "Course2", "Course3"];
 }
